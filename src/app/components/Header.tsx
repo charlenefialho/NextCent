372ff1bd0783684531966b35f1f32e59c110acee
charlenefialho <fialhocharlene@gmail.com>
2023-07-31 01:41:22 -0300

    feat: header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,19 +1,47 @@
 import Link from "next/link";
+import Image from "next/image";
 
-export function Header(){
-    return (
-        <header className="h-20 bg-white shadow ">
-            <nav className="w-3 py-3.5 py-4 md:flex md:items-center md:justify-between ">
-                <ul className="md:flex md:items-center md:justify-evenly">
-                    <li className="mx-4"><Link  className="text-xl" href="/"/>Home</li>
-                    <li className="mx-4"><Link href="/"/>Service</li>
-                    <li className="mx-4"><Link href="/"/>Feature</li>
-                    <li className="mx-4"><Link href="/"/>Product</li>
-                    <li className="mx-4"><Link href="/"/>Testimonial</li>
-                    <li className="mx-4"><Link href="/"/>FAQ</li>
-                </ul>
-            </nav>
-        </header>
-       
-    )
+import Logo from "../../../public/svg/logo.svg";
+import { Button } from "./Button";
+
+export function Header() {
+  return (
+    <header className="h-20 bg-white inline md:flex md:items-center">
+      <nav className="py-3.5 py-4  inline md:flex md:items-center md:justify-between cursor-pointer">
+        <div className="md:ml-24 ">
+          <Image className="inline" src={Logo} alt="logo" />
+        </div>
+        <ul className="md:flex md:items-center md:justify-evenly">
+          <li className="mr-12 md:my-0">
+            <Link className="text-xl" href="/" />
+            Home
+          </li>
+          <li className="mr-12 md:my-0">
+            <Link href="/" />
+            Service
+          </li>
+          <li className="mr-12 md:my-0">
+            <Link href="/" />
+            Feature
+          </li>
+          <li className="mr-12 md:my-0">
+            <Link href="/" />
+            Product
+          </li>
+          <li className="mr-12 md:my-0">
+            <Link href="/" />
+            Testimonial
+          </li>
+          <li className="mr-12 md:my-0">
+            <Link href="/" />
+            FAQ
+          </li>
+        </ul>
+      </nav>
+      <div className="flex gap-5 ">
+        <Button className="bg-green px-5 py-2.5 text-white">Login</Button>
+        <Button>Sign up</Button>
+      </div>
+    </header>
+  );
 }
