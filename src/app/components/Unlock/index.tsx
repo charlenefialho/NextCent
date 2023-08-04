cfd220a68d4c5c75168b99437f3ff49701457cb8
charlenefialho <fialhocharlene@gmail.com>
2023-08-04 06:47:13 -0300

    feat: add unlock

--- src/app/components/Unlock/index.tsx
+++ src/app/components/Unlock/index.tsx
@@ -1,35 +1,34 @@
 import Image from "next/image";
 
 import { Button } from "../Button";
-import imgUnlock from "../../../../public/svg/imageUnlock.svg";
 
-export function Unlock() {
+interface IUnlock{
+  imageUrl: string;
+  title:string;
+  description:string;
+
+}
+
+export function Unlock({imageUrl, title, description}: IUnlock) {
   return (
-    <section className="w-full h-screen mt-10 bg-neutral-silver flex justify-center items-center">
-      <div className="w-full flex items-center gap-28 -mt-20 h-4/5">
+    <section className="w-full max-w-[2560px] py-8 px-4 md:px-10 lg:px-20 xl:px-36 mt-10 bg-neutral-silver 2xl:justify-center 2xl:flex">
+      <div className="flex flex-col lg:flex-row items-center justify-center 2xl:w-4/6">
         <Image
-          className="hidden lg:block lg:ml-20"
-          src={imgUnlock}
+          className="w-full lg:w-auto h-auto max-w-[600px] mx-auto lg:mx-0 mb-6 lg:mb-0"
+          src={imageUrl}
           width={391}
           height={407}
           alt="Ilustração"
         />
-        <div className="flex  justify-center gap-28">
-          <div className="p-5  lg:p-10 flex flex-col  leading-10">
-            <h2 className=" font-semibold text-2xl md:text-3xl lg:text-4xl text-neutral-d-grey md:pr-20">
-              The unseen of spending three years at Pixelgrade
-            </h2>
-            <p className="text-neutral-grey text-sm md:text-base lg:text-lg pt-4 md:pr-20 xl:pr-72">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit
-              amet justo ipsum. Sed accumsan quam vitae est varius fringilla.
-              Pellentesque placerat vestibulum lorem sed porta. Nullam mattis
-              tristique iaculis. Nullam pulvinar sit amet risus pretium auctor.
-              Etiam quis massa pulvinar, aliquam quam vitae, tempus sem. Donec
-              elementum pulvinar odio.
-            </p>
-            <div className="mt-8">
-              <Button className="px-8 py-[14]">Register</Button>
-            </div>
+        <div className="flex flex-col justify-center">
+          <h2 className="font-semibold text-xl md:text-2xl lg:text-3xl text-neutral-d-grey mb-4">
+            {title}
+          </h2>
+          <p className="text-neutral-grey text-sm md:text-base lg:text-lg mb-6">
+           {description}
+          </p>
+          <div className="mt-4 flex justify-center min-[1024px]:justify-start">
+            <Button className="px-6 py-4">Register</Button>
           </div>
         </div>
       </div>
